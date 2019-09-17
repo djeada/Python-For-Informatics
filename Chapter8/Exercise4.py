696ee96695608eedc1c381be35d65f70aad29216
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-09-17 22:26:16 +0200

    Add files via upload

---
+++ Chapter8/Exercise4.py
@@ -0,0 +1,14 @@
+'''
+Exercise 4
+Modify find so that it has a third parameter, the index in word where it 
+should start looking.
+'''
+
+def find(word, letter, index):
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1
+
+print(find("xDDDDDDDDDDDDDD", "D", 3))
