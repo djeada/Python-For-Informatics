887564fdffcac4c37d2d7ab772ae1f678aed0c54
Adam Djellouli <01102712@pw.edu.pl>
2019-09-19 00:14:33 +0200

    Created a new folder

---
+++ Chapter9/Exercise3.py
@@ -0,0 +1,23 @@
+'''
+Exercise 3
+Write a program to read through a mail log, and build a histogram using a
+dictionary to count how many messages have come from each email address and
+print the dictionary.
+Enter file name: mbox-short.txt
+{'gopal.ramasammycook@gmail.com': 1, 'louis@media.berkeley.edu': 3,
+'cwen@iupui.edu': 5, 'antranig@caret.cam.ac.uk': 1,
+'rjlowe@iupui.edu': 2, 'gsilver@umich.edu': 3,
+'david.horwitz@uct.ac.za': 4, 'wagnermr@iupui.edu': 1,
+'zqian@umich.edu': 4, 'stephen.marquard@uct.ac.za': 2,
+'ray@media.berkeley.edu': 1}
+'''
+
+file = open('mbox-short.txt')
+lines = [line.strip('\n') for line in file if line.startswith('From ')]
+dictionary = {}
+
+for line in lines:
+    words = line.split()
+    dictionary[words[1]] = dictionary.get(words[1], 0) + 1
+
+print(dictionary)
---
+++ Chapter9/Exercise5.py
@@ -0,0 +1,21 @@
+'''
+Exercise 5
+This program records the domain name (instead of the address) where the
+message was sent from instead of who the mail came from (i.e. the whole
+e-mail address). At the end of the program print out the contents of your
+dictionary.
+python schoolcount.py
+Enter a file name: mbox-short.txt
+{'media.berkeley.edu': 4, 'uct.ac.za': 6, 'umich.edu': 7,
+'gmail.com': 1, 'caret.cam.ac.uk': 1, 'iupui.edu': 8}
+'''
+
+file_name = open('mbox-short.txt')
+lines = [line.strip("\n") for line in file_name if line.startswith("From") and not line.startswith("From:")]
+dictionary = {}
+
+for line in lines:
+    line = line.split()
+    dictionary[line[1].split("@")[1]] = dictionary.get(line[1].split("@")[1], 0) + 1
+
+print(dictionary)
